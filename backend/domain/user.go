1664d656ca0bb6225aab559add9f41bc159fee81
tomohiro tanaka <tomohiro.tanaka27@gmail.com>
2024-02-26 15:52:47 +0900

    Add user domain model with basic fields

---
+++ backend/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
