2ef76e9dd7c9f775219a9903f330aaa0821e3cfc
tomohiro tanaka <tomohiro.tanaka27@gmail.com>
2024-02-27 09:23:59 +0900

    Add new dependencies and mock UserRepository for user service

---
+++ backend/service/user/service.go
@@ -0,0 +1,62 @@
+package user
+
+import (
+	"echo-api/domain"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserRepository interface {
+	GetByEmail(user *domain.User, email string) error
+	Create(user *domain.User) error
+	Update(user *domain.User) error
+	Delete(user *domain.User) error
+}
+
+type Service struct {
+	userRepo UserRepository
+}
+
+func NewService(ur UserRepository) *Service {
+	return &Service{
+		userRepo: ur,
+	}
+}
+
+func (s *Service) SignUp(user domain.User) (domain.User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	newUser := domain.User{Email: user.Email, Password: string(hashedPassword), Name: user.Name}
+	err = s.userRepo.Create(&newUser)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return newUser, nil
+}
+
+func (s *Service) LogIn(user domain.User) (string, error) {
+	var u domain.User
+	err := s.userRepo.GetByEmail(&u, user.Email)
+	if err != nil {
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": u.ID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	return token.SignedString([]byte(os.Getenv("SECRET")))
+}
---
+++ backend/service/user/service_test.go
@@ -0,0 +1,64 @@
+package user_test
+
+import (
+	"echo-api/domain"
+	"echo-api/service/user"
+	"echo-api/service/user/mocks"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestSignUp(t *testing.T) {
+	mockRepo := new(mocks.UserRepository)
+	s := user.NewService(mockRepo)
+
+	mockUser := domain.User{Email: "test@example.com", Password: "password", Name: "Test User"}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(mockUser.Password), bcrypt.DefaultCost)
+	mockUserWithHash := domain.User{Email: "test@example.com", Password: string(hashedPassword), Name: "Test User"}
+
+	mockRepo.On("Create", mock.AnythingOfType("*domain.User")).Return(nil).Run(func(args mock.Arguments) {
+		arg := args.Get(0).(*domain.User)
+		*arg = mockUserWithHash
+	})
+
+	result, err := s.SignUp(mockUser)
+	assert.Nil(t, err)
+	assert.Equal(t, mockUserWithHash.Email, result.Email)
+	assert.NotEqual(t, "password", result.Password)
+}
+
+func TestLogIn(t *testing.T) {
+	mockRepo := new(mocks.UserRepository)
+	s := user.NewService(mockRepo)
+
+	mockUser := domain.User{Email: "test@example.com", Password: "password", Name: "Test User"}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(mockUser.Password), bcrypt.DefaultCost)
+	mockUserWithHash := domain.User{ID: 1, Email: "test@example.com", Password: string(hashedPassword), Name: "Test User"}
+
+	mockRepo.On("GetByEmail", mock.AnythingOfType("*domain.User"), mock.AnythingOfType("string")).Return(nil).Run(func(args mock.Arguments) {
+		arg := args.Get(0).(*domain.User)
+		*arg = mockUserWithHash
+	})
+
+	os.Setenv("SECRET", "secret")
+	tokenString, err := s.LogIn(mockUser)
+	assert.Nil(t, err)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	assert.Nil(t, err)
+	assert.True(t, token.Valid)
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	assert.True(t, ok)
+	assert.Equal(t, float64(mockUserWithHash.ID), claims["user_id"])
+	assert.WithinDuration(t, time.Now().Add(time.Hour*72), time.Unix(int64(claims["exp"].(float64)), 0), time.Second)
+}
